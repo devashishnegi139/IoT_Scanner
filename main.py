e12363ed46eb329881e9561cfc152d9f97485c3c
devashishnegi139 <devashishnegi113399@gmail.com>
2025-06-04 11:52:48 +0530

    created the main file

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+from scan_network import scan
+if __name__ == "__main__":
+    subnet = "192.168.233.0/24"
+    print("[*] Scanning network...")
+    devices = scan(subnet)
+
+    print("[*] Devices found:")
+    for dev in devices:
+        print(dev)
